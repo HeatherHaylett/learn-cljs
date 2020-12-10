0d44e26fe120b50dbc1da653a9ceb16857e80afe
Heather Haylett <heatherhaylett@gmail.com>
2020-12-09 20:46:31 -0600

    (feat) add and subtract

--- out/learn_cljs/core.cljs
+++ out/learn_cljs/core.cljs
@@ -1,3 +1,13 @@
 (ns learn-cljs.core)
 
-(println "Hello world!")
+;; add two numbers
+(defn add-two [x y]
+  (+ x y))
+
+(add-two 1 1) ;;=> 2
+
+;; subtract one number from another
+(defn subtract-nums [x y]
+  (- x y))
+
+(subtract-nums 10 7) ;;=> 3
