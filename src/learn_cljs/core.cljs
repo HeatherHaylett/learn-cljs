97b9b1a64e190c8930e8763bd26d2ffa70788527
Heather Haylett <heatherhaylett@gmail.com>
2020-12-09 20:44:00 -0600

    (feat) operators

--- src/learn_cljs/core.cljs
+++ src/learn_cljs/core.cljs
@@ -1,3 +1,19 @@
 (ns learn-cljs.core)
 
-(println "Hello world!")
+;; add two numbers
+(defn add-two [x y]
+  (+ x y))
+
+(add-two 1 1) ;;=> 2
+
+;; subtract one number from another
+(defn subtract-nums [x y]
+  (- x y))
+
+(subtract-nums 10 7) ;;=> 3
+
+;; multiply operands
+(defn multiply-nums [x y z]
+  (* x y z))
+
+(multiply-nums 2 2 4) ;;=> 16
